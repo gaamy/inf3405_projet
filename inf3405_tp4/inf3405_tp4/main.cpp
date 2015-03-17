587b67f9fb1eae6e5b164eb8d38f9c65d2c3e854
gaamy <gabriel.amyot@gmail.com>
2015-03-17 12:52:58 -0400

    Ajout des fichiers fournis

---
+++ inf3405_tp4/inf3405_tp4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  inf3405_tp4
+//
+//  Created by Gabriel Amyot on 2015-03-17.
+//  Copyright (c) 2015 1635535_1641161. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])     // insert code here...
+    std::cout << "Hello, World!\n";;;;;;;;;;
+    return 0;
+}
